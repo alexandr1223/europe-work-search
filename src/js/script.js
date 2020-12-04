5ad4e645d05731097d494aa9a8e807c1d43853c0
alexandr1223 <pogrebnhyak19999@gmail.com>
2020-12-04 18:22:44 +0200

    Push project

---
+++ src/js/script.js
@@ -0,0 +1,48 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    
+
+    window.onload = function () {
+      document.body.classList.add('loaded_hiding');
+      window.setTimeout(function () {
+        document.body.classList.add('loaded');
+        document.body.classList.remove('loaded_hiding');
+      }, 500);
+    }
+
+
+
+    $('img.img-svg').each(function(){
+      var $img = $(this);
+      var imgClass = $img.attr('class');
+      var imgURL = $img.attr('src');
+      $.get(imgURL, function(data) {
+        var $svg = $(data).find('svg');
+        if(typeof imgClass !== 'undefined') {
+          $svg = $svg.attr('class', imgClass+' replaced-svg');
+        }
+        $svg = $svg.removeAttr('xmlns:a');
+        if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+          $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+        }
+        $img.replaceWith($svg);
+      }, 'xml');
+    });
+})
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    const menu = document.querySelector('.navigation-menu')
+    menu.classList.toggle('navigation-menu_active')
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
